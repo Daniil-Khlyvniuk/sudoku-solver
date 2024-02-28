8749cda4cf17c591e49bd0606fd925ac6a72f00a
KhlyvniukDaniil <d.hlivniuk@istu.edu.ua>
2024-02-28 18:47:26 +0200

    feat: implemented tabs for select image

--- client/src/components/SelectSudokuForm/SelectSudokuForm.tsx
+++ client/src/components/SelectSudokuForm/SelectSudokuForm.tsx
@@ -1,8 +1,9 @@
 import { SyntheticEvent, useState } from 'react'
-import { Box, Tab } from '@mui/material'
+import { Box, Tab, Tabs } from '@mui/material'
 import { TabContext, TabList, TabPanel } from '@mui/lab'
 import { UploadFileFromDevice } from 'components/SelectSudokuForm/Tabs'
 import { TypesOfUserInput } from 'components/SelectSudokuForm/Tabs/values'
+import { colors } from 'assets/styles/colors'
 
 
 const SelectSudokuForm = () => {
@@ -12,23 +13,42 @@ const SelectSudokuForm = () => {
 		setValue(newValue)
 	}
 
+	const tabSx = {
+		color: colors.primary,
+		'&.Mui-selected': { color: colors.medium_secondary },
+		fontWeight: 'bold',
+		fontSize: 16
+	}
 
 	return (
 		<TabContext value={ value }>
 			<Box sx={ { borderBottom: 1, borderColor: 'divider' } }>
-				<TabList onChange={ handleChange } aria-label="lab API tabs example">
-					<Tab label={ TypesOfUserInput.upload } value={ TypesOfUserInput.upload }/>
-					<Tab label={ TypesOfUserInput.pasteLinkToImage } value={ TypesOfUserInput.pasteLinkToImage }/>
-					<Tab label={ TypesOfUserInput.takePhoto } value={ TypesOfUserInput.takePhoto }/>
+				<TabList
+					onChange={ handleChange }
+					variant={ 'scrollable' }
+					sx={{
+						'& .MuiTabs-indicator': {
+							bgcolor: colors.medium_secondary
+						}
+					}}
+				>
+					<Tab
+						sx={ tabSx }
+						label={ TypesOfUserInput.upload }
+						value={ TypesOfUserInput.upload }
+					/>
+					<Tab sx={ tabSx } label={ TypesOfUserInput.selectTestImages } value={ TypesOfUserInput.selectTestImages }/>
+					<Tab sx={ tabSx } label={ TypesOfUserInput.pasteLinkToImage } value={ TypesOfUserInput.pasteLinkToImage }/>
+					<Tab sx={ tabSx } label={ TypesOfUserInput.takePhoto } value={ TypesOfUserInput.takePhoto }/>
 				</TabList>
 			</Box>
 			<TabPanel value={ TypesOfUserInput.upload }>
 				<UploadFileFromDevice/>
 			</TabPanel>
+			<TabPanel value={ TypesOfUserInput.selectTestImages }>{ TypesOfUserInput.selectTestImages }</TabPanel>
 			<TabPanel value={ TypesOfUserInput.pasteLinkToImage }>Item Two</TabPanel>
 			<TabPanel value={ TypesOfUserInput.takePhoto }>Item Three</TabPanel>
 		</TabContext>
-
 	)
 }
 
--- client/src/components/SelectSudokuForm/Tabs/values.ts
+++ client/src/components/SelectSudokuForm/Tabs/values.ts
@@ -1,5 +1,6 @@
 export const enum TypesOfUserInput {
 	upload = 'Upload image from device',
 	pasteLinkToImage = 'Pass a lint to the sudoku image',
+	selectTestImages = 'Select test images',
 	takePhoto = 'Take a photo',
 }
